b74bf5e42fbf4ef952de544cdf25fecc21089a6e
tanqingcheng <tanqingcheng@yuaiweiwu.com>
2025-08-06 17:31:19 +0800

    add quick sort

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -36,6 +36,20 @@ def insert_sort(array):
             j -= 1
         array[j+1] = base
 
+def quick_sort(array, left, right):
+    if left >= right:
+        return
+    i, j = left, right
+    pivot = array[left]
+    while i < j:
+        while i < j and array[j] >= pivot:
+            j -= 1
+        while i < j and array[i] <= pivot:
+            i += 1
+        swap(array, i, j)
+    swap(array, i, left)
+    quick_sort(array, left, i-1)
+    quick_sort(array, i+1, right)
 
 
 if __name__ == '__main__':
@@ -43,5 +57,6 @@ if __name__ == '__main__':
     # result = sorted(test_list)
     # bubble_sort(test_list)
     # selection_sort(test_list)
-    insert_sort(test_list)
+    # insert_sort(test_list)
+    quick_sort(test_list, 0, len(test_list) - 1)
     print(test_list)
